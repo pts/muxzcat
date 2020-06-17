5a64641ba1b17579c50f6d4d8255e4cf7c4f6981
Peter Szabo <pts@fazekas.hu>
2020-06-17 17:31:34 +0200

    made javac optional in genjava.sh

--- genjava.sh
+++ genjava.sh
@@ -7,12 +7,16 @@ gcc -C -E -DCONFIG_LANG_JAVA "$@" -ansi -O2 -W -Wall -Wextra -Werror muaxzcat.c 
 <muaxzcat.java.tmp1 perl -0777 -pe 's@\A.*START_PREPROCESSED\s+@@s' >muaxzcat.java.tmp2
 # !! Reuse numeric constants.
 (<muaxzcat.java.tmp2 perl -0777 -pe 's@^#(?!!).*\n@@gm; s@^[ \t]*;[ \t]*\n@@mg; s@(\n[ t]*)(?=\n)@@g') >muaxzcat.java || exit "$?"
-BCP=
-test -f java102rt.jar && BCP='-bootclasspath java102rt.jar'
-# Works with Java 1.8.
-javac -Xlint:-options -target 1.1 -source 1.2 $BCP muaxzcat.java
+# Don't do it automatically.
 : eval 'perl -pe "s@^public class muaxzcat @public class muxzcat @" <muaxzcat.java >muxzcat.java'
-: javac -Xlint:-options -target 1.1 -source 1.2 $BCP muxzcat.java
-ls -l muaxzcat.class
+#perl -pe 's@^public class muaxzcat @public class muxzcat @' <muaxzcat.java >muxzcat.java
+if test "$1"; then
+  BCP=
+  test -f java102rt.jar && BCP='-bootclasspath java102rt.jar'
+  # Works with Java 1.8.
+  javac -Xlint:-options -target 1.1 -source 1.2 $BCP muaxzcat.java
+  : javac -Xlint:-options -target 1.1 -source 1.2 $BCP muxzcat.java
+  ls -l muaxzcat.class
+fi
 
 : genjava.sh OK.
